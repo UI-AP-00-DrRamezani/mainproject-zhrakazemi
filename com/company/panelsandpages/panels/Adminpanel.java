a8ee8aba665a5c7cd26c4680a282680453c0a546
zahra <zahra.k199002@gmail.com>
2022-04-06 15:56:26 +0430

    completing other functions on admin panel

--- com/company/panelsandpages/panels/Adminpanel.java
+++ com/company/panelsandpages/panels/Adminpanel.java
@@ -1,13 +1,12 @@
 package com.company.panelsandpages.panels;
 
 import com.company.MainMenu.OnlineShopPanel;
-import com.company.entity.classes.Admin;
-import com.company.entity.classes.AdminCommander;
-import com.company.entity.classes.Goods;
-import com.company.entity.classes.SalePerson;
+import com.company.entity.classes.*;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+
+import static com.company.MainMenu.OnlineShopPanel.buyers;
 
 public class Adminpanel {
     void menu() {
@@ -22,19 +21,19 @@ public class Adminpanel {
         int order = sc.nextInt();
         switch (order) {
             case 1:
-                //
+                chngeInfo();
                 break;
             case 2:
                 Adminpanel.addSeller(OnlineShopPanel.sellersSignUp());
                 break;
             case 3:
-                //
+                addGood(Admin.notConfirmedGoods);
                 break;
             case 4:
                 //
                 break;
             case 5:
-                //
+              deleteUser();
                 break;
             case 6:
                 //
@@ -65,27 +64,101 @@ public class Adminpanel {
             t--;
         }
     }
-
-    public static void addGood(ArrayList<Goods> addGoodRequest) {
-        System.out.println("for viewing the requests list press 1");
+public static void addGood(  ArrayList<Goods> notConfirmedGoods){
+        System.out.println("for viewing request list press one: ");
         Scanner sc = new Scanner(System.in);
         int one = sc.nextInt();
-        int i = 1;
-        if (one == 1) {
-            for (Goods a : addGoodRequest) {
-                System.out.println(i + ".  " + a.getName() + "  " + a.getBrand() + "   " + a.getSellerCompanyName() + "   " + a.getSellerName() + "   " + a.getPrice());
-                i++;
-            }
-            System.out.println("enter the number of requests you are going to accept now: ");
-            int t = sc.nextInt();
-            while (t > 0) {
-                System.out.println("for accepting  requests enter their numbers: ");
-                int number = sc.nextInt();
-                number = -1;
-                Admin.reallGoods.add(addGoodRequest.get(number));
-                t--;
+        int i=1;
+        if(one ==1){
+            for(Goods a: notConfirmedGoods)
+            System.out.println(i+". "+a.getName() + "   "+a.getBrand()+"   "+a.getPrice()+"  "+a.getSellerCompanyName()+"   "+a.getSellerName());
+            i++;
+        }
+        System.out.println("please tell me how many requests you are going to accept now");
+        int t = sc.nextInt();
+        while(t>0){
+            System.out.println("now please tell me which product you are going to add:");
+            System.out.println("1-Cell Phone\n2-Lop top\n3-Shirt\n4-Shoes\n5-Tv\n6-Refrigerator\n7-Oven\n8-Food");
+            int category = sc.nextInt();
+            switch (category){
+                case 1:
+                    System.out.println("please enter the number next to the product:");
+                    int number = sc.nextInt();
+                    number=number-1;
+                    notConfirmedGoods.get(number).setConfirmStatus(true);
             }
         }
+}
+public void deleteUser(){
+        System.out.println("for viewing users list press one:");
+        Scanner sc = new Scanner(System.in);
+        int one = sc.nextInt();
+        int i=1;
+        if(one ==1){
+            for(Buyer a : OnlineShopPanel.buyers){
+                System.out.println(i+". "+a.getUserName()+"   "+a.getName()+" "+a.getLastName()+"   "+a.getEmail()+"   "+a.getPhoneNumber());
+                i++;
+            }
+            System.out.println("for deleting a user , enter the number next to each one:");
+            int number = sc.nextInt();
+            number=number-1;
+            OnlineShopPanel.buyers.remove(number);
+        }
+}
+public void viewUser(){
+        //TODO clean console
+    for(Buyer a : buyers){
+        int i=1;
+                System.out.println(i+". "+a.getUserName()+"   "+a.getName()+" "+a.getLastName()+"   "+a.getEmail()+"   "+a.getPhoneNumber());
+                i++;
+            }
+    }
+
+void chngeInfo(){
+        String new1;
+        long new3;
+    System.out.println("I want to change my:");
+    System.out.println("1-Name \n2-LastName \n3-E_mail\n4-PhoneNumber\n5-Password");
+    Scanner sc = new Scanner(System.in);
+    int order = sc.nextInt();
+    switch (order){
+        case 1:
+            System.out.println("please enter your new name:");
+            new1= sc.nextLine();
+            Admin.newAdmin.setName(new1);
+            System.out.println("done!");
+            break;
+        case 2:
+            System.out.println("please enter your new lastname:");
+            new1= sc.nextLine();
+            Admin.newAdmin.setLastName(new1);
+            System.out.println("done!");
+            break;
+        case 3:
+            System.out.println("please enter your new Email adress: ");
+            new1= sc.nextLine();
+            Admin.newAdmin.setEmail(new1);
+            System.out.println("done!");
+            break;
+        case 4:
+            System.out.println("please enter your new phone number:");
+            new3= sc.nextLong();
+            Admin.newAdmin.setPhoneNumber(new3);
+            System.out.println("done!");
+            break;
+        case 5:
+            System.out.println("please enter your new password:");
+            new1= sc.nextLine();
+            System.out.println("please reenter your password: ");
+            String pass2 = sc.nextLine();
+            while (pass2 != new1) {
+                System.out.println("passwords dosent match , try again");
+                pass2 = sc.nextLine();
+            }
+            Admin.newAdmin.setPassword(new1);
+            System.out.println("done!");
+            break;
+
     }
 }
-
+}
