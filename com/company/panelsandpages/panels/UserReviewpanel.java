0d9cd32b718eb800732b287f773de60c40fd3aa0
zahra <zahra.k199002@gmail.com>
2022-04-07 22:53:09 +0430

    new changes on this function: checking if user have ever bough this good or not

--- com/company/panelsandpages/panels/UserReviewpanel.java
+++ com/company/panelsandpages/panels/UserReviewpanel.java
@@ -1,19 +1,30 @@
 package com.company.panelsandpages.panels;
 
+import com.company.entity.classes.Admin;
 import com.company.entity.classes.Buyer;
+import com.company.entity.classes.Goods;
 import com.company.entity.classes.UserReview;
 
 import java.util.Scanner;
 
 public class UserReviewpanel {
-    UserReviewpanel getInof(Buyer buyer) {
+    UserReviewpanel getInof(Buyer buyer, Goods good) {
         UserReview client = new UserReview();
-        System.out.println("please enter yor review here ");
-        Scanner sc = new Scanner(System.in);
-        String veiw = sc.nextLine();
-        client.setText(veiw);
-        client.setStatus("waiting");
-        return this;
+        boolean bought = false;
+        for (Goods a : buyer.boughtGoods) {
+            if (a == good) {
+                System.out.println("please enter yor review here ");
+                Scanner sc = new Scanner(System.in);
+                String veiw = sc.nextLine();
+                client.setText(veiw);
+                client.setStatus("waiting");
+                bought = true;
+            }
+        }
+        if (bought == true) {
+            return this;
+        } else {
+            return null;
+        }
     }
-
 }
