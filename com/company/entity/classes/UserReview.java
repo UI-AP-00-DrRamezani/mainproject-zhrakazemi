804a7731221d9f0ac25bd21fcc90c83bfbdc75c2
zahra <zahra.k199002@gmail.com>
2022-04-07 11:18:18 +0430

    User Review class

--- com/company/entity/classes/UserReview.java
+++ com/company/entity/classes/UserReview.java
@@ -6,22 +6,9 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class UserReview {
-    private GeneralAccount user;
-    private String Text;
-    private String status;
-    ArrayList<UserReview> clientsView = new ArrayList<UserReview>();
-
-    UserReview getInof() {
-        UserReview client = new UserReview();
-        // setUser(//TODO تابع لاگین ک یک جنرال اکانت برمیگرداند));
-        System.out.println("please enter yor review here ");
-        Scanner sc = new Scanner(System.in);
-        String veiw = sc.nextLine();
-        setText(veiw);
-        setStatus("waiting");
-        clientsView.add(client);
-        return this;
-    }
+     GeneralAccount user;
+     String Text;
+     String status;
 
     public GeneralAccount getUser() {
         return user;
