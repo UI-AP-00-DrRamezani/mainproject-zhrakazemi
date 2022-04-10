c0128933c5d33dbb5a7dcae001b09136e10ded19
zahra <zahra.k199002@gmail.com>
2022-04-10 16:20:15 +0430

    new changes: adding a condition to check if the user has logged in or not

---
+++ com/company/panelsandpages/panels/ProductPanel.java
@@ -0,0 +1,57 @@
+package com.company.panelsandpages.panels;
+
+import com.company.Main;
+import com.company.MainMenu.OnlineShopPanel;
+import com.company.entity.classes.Buyer;
+import com.company.entity.classes.CellPhone;
+import com.company.entity.classes.Goods;
+
+import java.util.Scanner;
+
+public class ProductPanel {
+    public static void showProduct( Goods goods ){
+        while (true) {
+            System.out.println("------------------------");
+            System.out.println(goods.toString());
+            System.out.println("------------------------");
+            System.out.println("press:\n1-add this product to your cart\n2-write a review on this product\n3-give an score to this product\n4-to view your cart");
+            Scanner sc = new Scanner(System.in);
+            int choose = sc.nextInt();
+            switch (choose) {
+                case 1:
+                    if(Main.buyer != null){
+                        Main.buyer.cart.add(goods);}
+                    else{
+                    System.out.println("sorry , you must have login first: ");
+                      OnlineShopPanel.buyerlogin();}
+                    break;
+                case 2:
+                    if(Main.buyer != null){
+                        UserReviewpanel.getInof(Main.buyer, goods);
+                    }
+                    else {
+                    System.out.println("sorry , you must log in first");
+                    OnlineShopPanel.buyerlogin();
+                }
+                    break;
+                case 3:
+                    if(Main.buyer != null){
+                    UserScorePanel.getScore(goods);}
+                    else {
+                    System.out.println("sorry , you must log in first");
+                    OnlineShopPanel.buyerlogin();
+                }
+                    break;
+                case 4:
+                    if(Main.buyer != null) {
+                        BuyerPanel.cart(Main.buyer);
+                    }
+                    else {
+                        System.out.println("sorry , you must log in first");
+                        OnlineShopPanel.buyerlogin();
+                    }
+                    break;
+            }
+        }
+    }
+}
