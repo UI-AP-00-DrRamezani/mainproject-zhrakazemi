e2105fe28984aa673cc07d9141124774e19e9050
zahra <zahra.k199002@gmail.com>
2022-04-11 17:16:24 +0430

    clean up code

--- com/company/panelsandpages/panels/UserScoreCommander.java
+++ com/company/panelsandpages/panels/UserScoreCommander.java
@@ -1,19 +1,12 @@
 package com.company.panelsandpages.panels;
 
-import com.company.entity.classes.Buyer;
 import com.company.entity.classes.Goods;
-import com.company.entity.classes.UserScores;
-
-import java.util.ArrayList;
-import java.util.Scanner;
 
 public class UserScoreCommander {
-
-
-  public static   void countScore(Goods goods) {
+    public static void countScore(Goods goods) {
         double sum = 0;
         int counter = 0;
-        for (Double a : goods.clientsScore ) {
+        for (Double a : goods.clientsScore) {
             sum += a;
             counter++;
         }
