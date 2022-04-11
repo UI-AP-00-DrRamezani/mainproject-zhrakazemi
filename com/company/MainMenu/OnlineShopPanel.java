17038f915388a05dd4c79b16c1a34384bbb23c77
zahra <zahra.k199002@gmail.com>
2022-04-11 17:03:45 +0430

    fixing some login and signup problems

--- com/company/MainMenu/OnlineShopPanel.java
+++ com/company/MainMenu/OnlineShopPanel.java
@@ -1,5 +1,6 @@
 package com.company.MainMenu;
 
+import com.company.Main;
 import com.company.entity.classes.Admin;
 import com.company.entity.classes.Buyer;
 import com.company.entity.classes.Goods;
@@ -11,17 +12,17 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class OnlineShopPanel {
-   public static ArrayList<Buyer> buyers = new ArrayList<Buyer>();
-  public static  ArrayList<SalePerson> sellers = new ArrayList<SalePerson>();
-public static ArrayList<Goods> goods = new ArrayList<Goods>();
+    public static ArrayList<Buyer> buyers = new ArrayList<Buyer>();
+    public static ArrayList<SalePerson> sellers = new ArrayList<SalePerson>();
+    public static ArrayList<Goods> goods = new ArrayList<Goods>();
 
-  public static   void clientSignUp() {
+    public static void clientSignUp() {
         System.out.println("please enter your information: ");
         Scanner sc = new Scanner(System.in);
         System.out.println("please choose an ID: ");
         String userName = sc.nextLine();
         for (Buyer a : buyers) {
-            while (userName ==a.getUserName()) {
+            while (userName.equals(a.getUserName())) {
                 System.out.println("already taken , please choose another username");
                 userName = sc.nextLine();
             }
@@ -34,28 +35,33 @@ public static ArrayList<Goods> goods = new ArrayList<Goods>();
         String email = sc.nextLine();
         System.out.println("enter your phone number: ");
         long phonenumber = sc.nextLong();
+        System.out.println("please enter how much money you want to put in your account: ");
+        long money = sc.nextLong();
         System.out.println("well done! \n now please choose an strong password: ");
+        sc.nextLine();
         String password = sc.nextLine();
         System.out.println("please reenter your password: ");
         String pass2 = sc.nextLine();
-        while (pass2 != password) {
+        while (!pass2.equals(password)) {
             System.out.println("passwords dosent match , try again");
             pass2 = sc.nextLine();
         }
         System.out.println("done! for entering System press one: ");
         int one = sc.nextInt();
+        Buyer buyer = new Buyer(userName, name, lastName, email, phonenumber, password, money);
+        buyers.add(buyer);
         if (one == 1) {
-            login();
+            OnlineShopPanel.buyerlogin();
         }
     }
 
-   public static ArrayList<SalePerson> sellersSignUp() {
+    public static ArrayList<SalePerson> sellersSignUp() {
         System.out.println("please enter your information: ");
         Scanner sc = new Scanner(System.in);
         System.out.println("please choose an ID: ");
         String userName = sc.nextLine();
         for (SalePerson a : Admin.reallSellers) {
-            while (userName == a.getUserName()) {
+            while (userName.equals(a.getUserName())) {
                 System.out.println("already taken , please choose another username");
                 userName = sc.nextLine();
             }
@@ -73,54 +79,62 @@ public static ArrayList<Goods> goods = new ArrayList<Goods>();
         System.out.println("enter your phone number: ");
         long phonenumber = sc.nextLong();
         System.out.println("well done! \n now please choose an strong password: ");
+        sc.nextLine();
         String password = sc.nextLine();
         System.out.println("please reenter your password: ");
         String pass2 = sc.nextLine();
-        while (pass2 != password) {
+        while (!pass2.equals(password)) {
             System.out.println("passwords dosent match , try again");
             pass2 = sc.nextLine();
         }
         SalePerson seller = new SalePerson(userName, name, lastName, email, phonenumber, password, companyName, companyNo);
         sellers.add(seller);
-        return  sellers;
+        return sellers;
     }
 
-    public static void login() {
-        System.out.println("please choose which one are you  ");
-        System.out.println("1- Client \n2-Salesperson");
+    public static Buyer buyerlogin() {
+        Buyer[] newbuyer = new Buyer[1];
         Scanner sc = new Scanner(System.in);
-        int role = sc.nextInt();
-        if (role == 1) {
-            System.out.println("if you dont have account press 0 to sign up:");
-            int zero = sc.nextInt();
-            if (zero == 0) {OnlineShopPanel.clientSignUp();}
-                System.out.println("please enter your username: ");
-                String userName = sc.nextLine();
-                for (Buyer a : buyers) {
-                    if (userName == a.getUserName()) {
-                        System.out.println("please enter your password: ");
-                        String password = sc.nextLine();
-                        if (password == a.getPassword()) {
-                            BuyerPanel.panel(a);
-                        }
-                    }
+        System.out.println("please enter your username: ");
+        String userName = sc.nextLine();
+        for (Buyer a : buyers) {
+            if (a.getUserName().equals(userName)) {
+                System.out.println("please enter your password: ");
+                String password = sc.nextLine();
+                if (password.equals(a.getPassword())) {
+                    newbuyer[0] = a;
+                    Main.buyer = a;
+                    BuyerPanel.panel(a);
+                } else {
+                    return null;
                 }
-            } else if (role == 2) {
-            System.out.println("if you dont have account press 0 to sign up:");
-            int zero = sc.nextInt();
-            if (zero == 0) {
-                OnlineShopPanel.sellersSignUp();}
-                System.out.println("please enter your username: ");
-                String userName = sc.nextLine();
-                for (SalePerson a : Admin.reallSellers) {
-                    if (userName == a.getUserName()) {
-                        System.out.println("please enter your password: ");
-                        String password = sc.nextLine();
-                        if (password == a.getPassword()) {
-                            SellerPanel.panel(a);
-                        }
+            } else {
+                return null;
+            }
+        }
+        return newbuyer[0];
+    }
+
+    public static void sellerLogin() {
+        System.out.println("if you dont have account press 0 to sign up and if you want to log in to your account press 1:");
+        Scanner sc = new Scanner(System.in);
+        int zero = sc.nextInt();
+        if (zero == 0) {
+            OnlineShopPanel.sellersSignUp();
+        } else if (zero == 1) {
+            sc.nextLine();
+            System.out.println("please enter your username: ");
+            String userName = sc.nextLine();
+            for (SalePerson a : Admin.reallSellers) {
+                if (userName.equals(a.getUserName())) {
+                    System.out.println("please enter your password: ");
+                    String password = sc.nextLine();
+                    if (password.equals(a.getPassword())) {
+                        SellerPanel.panel(a);
                     }
                 }
             }
         }
     }
+
+}
