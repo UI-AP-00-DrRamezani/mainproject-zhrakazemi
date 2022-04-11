648b79c7dc6b735343e22184e4c9e7cf0642b594
zahra <zahra.k199002@gmail.com>
2022-04-11 16:51:26 +0430

    Buyer class and Buyer panel

--- com/company/entity/classes/Buyer.java
+++ com/company/entity/classes/Buyer.java
@@ -2,26 +2,21 @@ package com.company.entity.classes;
 
 import java.util.ArrayList;
 
-public class Buyer extends GeneralAccount{
-    ArrayList<Goods> cart = new ArrayList<Goods>();
-    //ArrayList<Goods> cart = new ArrayList<Goods>();
+public class Buyer extends GeneralAccount {
+    public ArrayList<Goods> cart = new ArrayList<Goods>();
+    public ArrayList<Goods> boughtGoods = new ArrayList<Goods>();
+    private long money;
 
-    public Buyer(String userName, String name, String lastName, String email, long phoneNumber, String password, long money, long budget) {
+    public Buyer(String userName, String name, String lastName, String email, long phoneNumber, String password, long money) {
         super(userName, name, lastName, email, phoneNumber, password);
-        this.budget = budget;
+        setMoney(money);
     }
 
-    //TODO سابقه خرید
-    long budget;
-    void shopping (){
-        int number = cart.size();
-        for(int i=0 ; i<number ; i++){
-            if (budget > cart.get(i).getPrice()){
-                budget-=cart.get(i).getPrice();
-                //TODO اضافه شدن به فاکتور
-                //TODO اضافه شدن به سابقه خرید
-            }
-        }
+    public long getMoney() {
+        return money;
+    }
 
+    public void setMoney(long money) {
+        this.money = money;
     }
 }
--- com/company/panelsandpages/panels/BuyerPanel.java
+++ com/company/panelsandpages/panels/BuyerPanel.java
@@ -1,30 +1,31 @@
 package com.company.panelsandpages.panels;
 
-import com.company.entity.classes.Admin;
+import com.company.Main;
 import com.company.entity.classes.Buyer;
-import com.company.entity.classes.GeneralAccount;
-import com.company.MainMenu.OnlineShopPanel;
 import com.company.entity.classes.Goods;
 
 import java.util.ArrayList;
 import java.util.Scanner;
 
 import static com.company.MainMenu.OnlineShopPanel.buyers;
-import static com.company.MainMenu.OnlineShopPanel.goods;
 
 public class BuyerPanel {
-    public static void panel(Buyer buyer){
-System.out.println("if you want to change your information, press one");
-        Scanner sc = new Scanner(System.in);
-        int one = sc.nextInt();
-        if(one==1){
-            changeInfo(buyer);
+    public static void panel(Buyer buyer) {
+        while (true) {
+            System.out.println("if you want to change your information, press one and if you want to see products , press 0 \n 2-log out");
+            Scanner sc = new Scanner(System.in);
+            int one = sc.nextInt();
+            if (one == 1) {
+                changeInfo(buyer);
+            } else if (one == 2) {
+                Main.buyer = null;
+            } else if (one == 0) {
+                CategoryPanel.manu();
+            }
         }
-        else{
-          CategoryPanel.showProduct();
-           }
-        }
-   public static void changeInfo(Buyer buyer){
+    }
+
+    public static void changeInfo(Buyer buyer) {
         String new1;
         long new3;
         System.out.println("I want to change my:");
@@ -32,58 +33,88 @@ System.out.println("if you want to change your information, press one");
         Scanner sc = new Scanner(System.in);
         int order = sc.nextInt();
         int i = buyers.indexOf(buyer);
-        switch (order){
+        switch (order) {
             case 1:
                 System.out.println("please enter your new name:");
-                 new1= sc.nextLine();
+                sc.nextLine();
+                new1 = sc.nextLine();
                 buyers.get(i).setName(new1);
                 System.out.println("done!");
                 break;
             case 2:
                 System.out.println("please enter your new lastname:");
-                new1= sc.nextLine();
+                sc.nextLine();
+                new1 = sc.nextLine();
                 buyers.get(i).setLastName(new1);
                 System.out.println("done!");
                 break;
             case 3:
                 System.out.println("please enter your new Email adress: ");
-                new1= sc.nextLine();
+                sc.nextLine();
+                new1 = sc.nextLine();
                 buyers.get(i).setEmail(new1);
                 System.out.println("done!");
                 break;
             case 4:
                 System.out.println("please enter your new phone number:");
-                new3= sc.nextLong();
-              buyers.get(i).setPhoneNumber(new3);
+                new3 = sc.nextLong();
+                buyers.get(i).setPhoneNumber(new3);
                 System.out.println("done!");
                 break;
             case 5:
                 System.out.println("please enter your new password");
-                new1= sc.nextLine();
+                sc.nextLine();
+                new1 = sc.nextLine();
                 System.out.println("please reenter your password: ");
                 String pass2 = sc.nextLine();
                 while (pass2 != new1) {
                     System.out.println("passwords dosent match , try again");
                     pass2 = sc.nextLine();
                 }
-               buyers.get(i).setPassword(new1);
+                buyers.get(i).setPassword(new1);
                 System.out.println("done!");
                 break;
 
         }
     }
 
-    public static void printFacture(ArrayList<Goods> goods){
-        System.out.println("facture No." +BuyerCommander.getFactureCod()+"      "+"Date: " + BuyerCommander.getFactureDate());
+    public static void printFacture(ArrayList<Goods> goods) {
+        long price = 0;
+        for (Goods a : goods) {
+            price = a.getPrice() + price;
+        }
+        String sellerName = goods.get(0).getSellerName();
+        Facture newFacture = new Facture("m145g", price, sellerName, "waiting");
+        System.out.println("facture No." + newFacture.getFactureCod() + "      " + "Date: " + newFacture.getFactureDate());
         System.out.println("--------------------products list-------------------");
-        int i=1;
+        int i = 1;
         System.out.println();
-        for(Goods a :goods){
-            System.out.println(i +" ."+ a.getName() +"   "+a.getBrand()+"   "+a.getPrice()+"    "+a.getSellerCompanyName()+"   "+a.getSellerName());
+        for (Goods a : goods) {
+            System.out.println(i + " ." + a.getName() + "   " + a.getBrand() + "   " + a.getPrice() + "    " + a.getSellerCompanyName() + "   " + a.getSellerName());
         }
         System.out.println("----------------------------------------------------");
 
-        System.out.println("send statuse: "+ BuyerCommander.getFactureSendStatus());
+        System.out.println("send statuse: " + newFacture.getFactureSendStatus());
+    }
+
+    public static void cart(Buyer buyer) {
+        System.out.println("------------------");
+        int number = buyer.cart.size();
+        System.out.println("number of products: " + number);
+        int i = 1;
+        System.out.println();
+        for (Goods a : buyer.cart) {
+            System.out.println(i + " ." + a.getName() + "   " + a.getBrand() + "   " + a.getPrice() + "    " + a.getSellerCompanyName() + "   " + a.getSellerName());
+        }
+        System.out.println("------------------");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("to finish your shopping press 1 and for viewing products press0: ");
+        int order = sc.nextInt();
+        if (order == 1) {
+            FactureCommander.shopping(buyer);
+        } else if (order == 0) {
+            CategoryPanel.manu();
+        }
     }
 }
 
